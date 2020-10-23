3b5d0bb8a24a4422c0ca4472261223b26352b85c
Blaiseniyo <blaiseniyonkuru12@gmail.com>
2020-10-23 20:20:25 +0200

    making the signin page responsive to all device

--- JS/signin.js
+++ JS/signin.js
@@ -6,7 +6,7 @@ const navSlide = () => {
 
     burger.addEventListener('click', () => {
         //Toggle nav
-        //document.querySelector('.form-container').classList.toggle('hide');
+        document.querySelector('.form-container').classList.toggle('hide');
         nav.classList.toggle('nav-active')
             //Animate links
     navLinks.forEach((link,index) => {
