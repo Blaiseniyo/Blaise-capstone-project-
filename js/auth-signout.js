5d5aa228e679c58abee9acd948842317e835438d
Blaiseniyo <blaiseniyonkuru12@gmail.com>
2020-11-13 11:37:22 +0200

    adding auth valiable on all pages

--- js/auth-signout.js
+++ js/auth-signout.js
@@ -1,3 +1,11 @@
+auth.onAuthStateChanged(user =>{
+    if(user){
+        console.log('uesr logged in');
+    }else{
+        console.log('uesr logged out');
+    }
+})
+
 document.getElementById('log-out').addEventListener('click', (e)=>{
     e.preventDefault()
     auth.signOut().then(()=>{
