e7044b12d96871aa9915b620a76139faca97884c
Blaiseniyo <blaiseniyonkuru12@gmail.com>
2020-11-13 19:09:28 +0200

    adding restricting on some pages

--- JS/auth2.js
+++ JS/auth2.js
@@ -1,17 +1,20 @@
-let url =location.href;
+let ur =location.hash.slice(1);
 document.getElementById('log-out').addEventListener('click', (e)=>{
     e.preventDefault()
     auth.signOut().then(()=>{
         console.log('clicked'); 
     });
 })
-
 auth.onAuthStateChanged(user =>{
     if(user){
         setRules(user);
-        console.log('uesr logged in');
+        console.log('user logged in');
+        if(ur){
+            location.href=ur;
+        }else{
+            // location.href
+        }
     }else{
         setRules();
-        window.location.href=`signin.html#${url}`;
     }
 })
