1767b4747eae0ea69107fda4d1b22f2626d0e11d
Blaiseniyo <blaiseniyonkuru12@gmail.com>
2020-11-17 06:44:38 +0200

    changing a valiable name

--- js/projects.js
+++ js/projects.js
@@ -1,5 +1,8 @@
 let form3 =document.querySelector('.project-form');
 
+let changed1 = false;
+let fil1 =null;
+
 function renderProjects(doc){
     let div =document.createElement('div');
     let i =document.createElement('img');
@@ -12,6 +15,8 @@ function renderProjects(doc){
     i.style.width='140px';
     i.style.height='120px';
     h5.textContent=doc.data().name;
+    text.cols='6';
+    text.rows='8m';
     text.value=doc.data().description;
     btn.classList.add('remove');
     btn.textContent="remove";
@@ -46,22 +51,37 @@ db.collection('projects').onSnapshot(snapshot =>{
 function renderNewproject(){
     let form=document.createElement('form');
     let input1 = document.createElement('input');
+    let link = document.createElement('input');
     let input = document.createElement('input');
     let des =document.createElement('textarea');
     des.cols='6';
     des.rows='8';
+    input1.style.marginTop="10px";
     input1.style.marginBottom="10px";
     input.style.marginBottom="10px";
     input1.type='text';
+    link.type='text';
+    input1.name='name';
+    link.name='link';
+    input.name='image';
+    input.classList.add('image');
+    des.name='description';
+    form.classList.add('in')
     input1.placeholder='Title';
     input.type='file';
 
     form.appendChild(input1);
     form.appendChild(des);
+    form.appendChild(link);
     form.appendChild(input);
 
     form3.appendChild(form);
 
+
+    input.addEventListener('change', (e)=>{
+        changed1=true;
+        fil1= e.target.files[0];
+    })
     console.log();
 }
 
@@ -73,6 +93,27 @@ document.querySelector('.add-2').addEventListener('click',(e)=>{
 
 document.querySelector('.save-3').addEventListener('click',(e)=>{
     document.querySelector('.add-2').style.display='block';
-    document.querySelector('.save-3').style.display="none"
+    document.querySelector('.save-3').style.display="none";
+    let form = document.querySelector('.in');
+    let file=document.querySelector('.image');
+
+    if(changed){
+        let storageRef =str.ref('image/'+fil1.name);
+        storageRef.put(fil1).then(()=>{
+            storageRef.getDownloadURL().then(url=>{
+                db.collection('projects').add({
+                    name: form.name.value,
+                    description:form.description.value,
+                    link: form.link.value,
+                    image:url
+            }).then(()=>{
+                console.log(added);
+            })
+        })
+    })
+    }else{
+        alert('please you have to first choice an Image for your project');
+    }
+
 })
 
