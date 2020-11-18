0067e00507b4af51c0cd0606853eb0d76b05358f
Blaiseniyo <62561244+Blaiseniyo@users.noreply.github.com>
2020-11-18 14:26:09 +0200

    reading articles from  firebase

--- JS/article.js
+++ JS/article.js
@@ -22,4 +22,66 @@ const navSlide = () => {
     });
 }
 
+let id=location.hash.slice(1);
+function renderArticle(article){
+    const title=document.querySelector('#title');
+    const content=document.querySelector('#content');
+    const image=document.querySelector('.image');
+    title.textContent=article.title;
+    content.textContent=article.content;
+    console.log(article.image);
+    image.src=article.image;
+    image.style.height='400px'
+}
+
+db.collection('posts').doc(id).get().then((article)=>{
+    renderArticle(article.data());
+}) 
+
+
+function renderComments(comment){
+    const comments =document.querySelector('#comment-1');
+    const name =document.createElement('h2');
+    const com =document.createElement('p');
+    const div =document.createElement('div');
+    div.setAttribute('class','comment');
+    name.textContent=comment.name;
+    com.textContent=comment.comment;
+    div.appendChild(name);
+    div.appendChild(com);
+    comments.appendChild(div);
+}
+
+//displaying the comment to the UI
+
+db.collection('comments').where("post","==",id).onSnapshot(snpashot =>{
+    let changes =snpashot.docChanges();
+    changes.forEach(change =>{
+        if(change.type=='added'){
+            renderComments(change.doc.data());
+        }
+        else if(change.type== 'removed'){
+            // let art = posts.querySelector('[post-id='+ change.doc.id +']');
+            // posts.removeChild(art);
+        }
+    })
+})
+
+let form =document.querySelector('.comment-form');
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    if( form.name.value && form.comment.value){
+        db.collection('comments').add({
+            post: id,
+            name:form.name.value,
+            comment: form.comment.value
+        })
+        form.name.value=' ';
+        form.comment.value=' ';
+    }else{
+        alert('Enter your name and your comment before you leave a comment ');
+    }
+})
+
 navSlide();
